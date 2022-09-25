7a92e8488643e7a8b399d637fae34be6ed2455ad
The_ai <sans34678@gmail.com>
2022-09-25 19:04:21 -0400

    version 1 Brute force works

--- prime.py
+++ prime.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 prime = 82589933
+#prime = 17
 
 def checkprime(number):
     n=2
@@ -9,16 +10,19 @@ def checkprime(number):
             n = n + 1
         else:
             n = n + 1
-        #print(n)
 
 def bruteforce(number):
-    i=2
     for i in range(2,number):
         if number%i==0:
             return("not prime")
+    return("prime")
 
 
 
-x = time.process_time()
-print(bruteforce(prime))
-print(checkprime(prime))
+
+if __name__ == "__main__":
+    x = time.process_time()
+    print(bruteforce(prime))
+    print(str(time.process_time()-x) + " seconds")
+    print(checkprime(prime))
+    print(str(time.process_time() - x) + " seconds")
