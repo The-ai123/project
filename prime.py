8cbb145a54b916604430c9ee47372f860a39aa16
The_ai <sans34678@gmail.com>
2022-10-10 11:52:23 -0400

    version 2 Added Several new methods

--- prime.py
+++ prime.py
@@ -1,7 +1,8 @@
 import numpy as np
 import time
+import math
 prime = 82589933
-#prime = 17
+prime = 19999987
 
 
 def halfway(number):
@@ -10,6 +11,8 @@ def halfway(number):
     :param number: number to be checked
     :return: True or false based if its prime or not
     """
+    if number < 3:
+        return True
     for i in range(2,number//2):
         if number%i==0:
             return(False)
@@ -21,6 +24,8 @@ def bruteforce(number):
     :param number: number to be checked
     :return: True or false based if its prime or not
     """
+    if number < 3:
+        return True
     for i in range(2,number):
         if number%i==0:
             return(False)
@@ -33,6 +38,8 @@ def oddsonly(number):
     :param number: number to be checked
     :return: True or false based if its prime or not
     """
+    if number < 3:
+        return True
     if number % 2 == 0:
         return False
     for i in range(3,number//2,2):
@@ -67,10 +74,67 @@ def skipmultiples(number):
     n += 4
     if number % n == 0:
         return False
+
     return (True)
+
+def generateprimedistance(num):
+    l = []
+    for i in range(num):
+        l.append((i,oddsonly(i)))
+    print(l)
+    dist = []
+    count = 1
+    for i in l:
+        if i[1] == True:
+            dist.append((i[0],count))
+            count = 1
+        else:
+            count+=1
+    print(dist)
+
+def generateprimes(num):
+    l = []
+    for i in range(2,num):
+        if oddsonly(i):
+            l.append(i)
+    return(l)
+
+
+def pregenprime(number):
+    """
+    Pregenerates a list of primes to check this probably is slower and doesn't work
+    :param number: number to be checked
+    :return: True or false based if its prime or not
+    """
+    l = generateprimes(math.floor(math.sqrt(number)))
+    for i in l:
+        if number%i == 0:
+            return False
+    return True
+
+def squareroot(number):
+    """
+    aaaaaaaaaaaaaaaaaa
+    :param number: number to be checked
+    :return: True or false based if its prime or not
+    """
+    if number < 3:
+        return True
+    if number % 2 == 0:
+        return False
+    for i in range(3,math.ceil(math.sqrt(number)),2):
+        if number%i==0:
+            return(False)
+    return(True)
+
+def readlist():
+    f = open("primes.txt")
+    print(f.read().split())
+
 
 
 if __name__ == "__main__":
+    primelist = readlist()
     print("False means NOT prime and True means it is prime")
     x = time.process_time()
     print("Brute Force Method: " + str(bruteforce(prime)) + " | Time taken: " + str(time.process_time() - x) + " seconds")
@@ -79,7 +143,7 @@ if __name__ == "__main__":
     x = time.process_time()
     print("Odds Only Method: " + str(oddsonly(prime)) + " | Time taken: " + str(time.process_time() - x) + " seconds")
     x = time.process_time()
-    print("Skip Multiples Method: " + str(skipmultiples(prime)) + " | Time taken: " + str(time.process_time()-x) + " seconds")
+    print("Pregenerate primes: " + str(pregenprime(prime)) + " | Time taken: " + str(time.process_time()-x) + " seconds")
     x = time.process_time()
-
+    print("Square Root Method: " + str(squareroot(prime)) + " | Time taken: " + str(time.process_time() - x) + " seconds")
 
