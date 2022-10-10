21c553ef070cdf764b0bdd9e31e8554dfff13b73
The_ai <sans34678@gmail.com>
2022-10-10 11:02:41 -0400

    version 1 Added Odds only method

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 import numpy as np
 import time
 prime = 82589933
-prime = 17
+#prime = 17
 
 
 def halfway(number):
@@ -26,6 +26,20 @@ def bruteforce(number):
             return(False)
     return(True)
 
+
+def oddsonly(number):
+    """
+    cuts checks in half again by only checking odds
+    :param number: number to be checked
+    :return: True or false based if its prime or not
+    """
+    if number % 2 == 0:
+        return False
+    for i in range(3,number//2,2):
+        if number%i==0:
+            return(False)
+    return(True)
+
 def skipmultiples(number):
     """
     if a number is not divisible by a number, it is also not divisible any multiples of that same number
@@ -33,20 +47,39 @@ def skipmultiples(number):
     :return: True or false based if its prime or not
     """
     n = 2
-    while n < (number//2):
-        if number%n == 0:
-            return (False)
-        n+=1
-        print(n)
+    if number%n == 0:
+        return False
+    n+=1
+    if number % n == 0:
+        return False
+    n+=2
+    if number % n == 0:
+        return False
+    n+=2
+    if number % n == 0:
+        return False
+    n += 4
+    if number % n == 0:
+        return False
+    n += 2
+    if number % n == 0:
+        return False
+    n += 4
+    if number % n == 0:
+        return False
     return (True)
 
 
 if __name__ == "__main__":
     print("False means NOT prime and True means it is prime")
     x = time.process_time()
-    print("Half Way Method: " + str(halfway(prime)) + "| Time taken: " + str(time.process_time()-x) + " seconds")
+    print("Brute Force Method: " + str(bruteforce(prime)) + " | Time taken: " + str(time.process_time() - x) + " seconds")
     x = time.process_time()
-    print("Skip Multiples Method: " + str(skipmultiples(prime)) + "| Time taken: " + str(time.process_time()-x) + " seconds")
+    print("Half Way Method: " + str(halfway(prime)) + " | Time taken: " + str(time.process_time()-x) + " seconds")
+    x = time.process_time()
+    print("Odds Only Method: " + str(oddsonly(prime)) + " | Time taken: " + str(time.process_time() - x) + " seconds")
+    x = time.process_time()
+    print("Skip Multiples Method: " + str(skipmultiples(prime)) + " | Time taken: " + str(time.process_time()-x) + " seconds")
     x = time.process_time()
 
 
